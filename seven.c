7e4482900ac65bd86f02bbd067564dc4984aebda
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2022-10-24 10:37:45 -0400

    Update seven.c

--- seven.c
+++ seven.c
@@ -2,6 +2,8 @@
 
 int main() {
   printf("%d\n", 3 * 7);
-  printf("it's ugly, and it's hard to divide by\n");
+  for (int increment = 0; increment < 7; increment++) {
+    printf("it's ugly, and it's hard to divide by\n");
+  }
   return 7 - 7;
 }
